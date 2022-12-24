10402741ccfda9c800936426e2f5c22821845fd3
MrProgrammer00 <88051679+MrProgrammer00@users.noreply.github.com>
2022-12-24 08:37:42 +0530

    yes

--- main.js
+++
@@ -1,8 +0,0 @@
-/*function imgSlider(anything) {
-    document.querySelector('.backimg').src = anything;
-}
-
-function changeCircleColor(color) {
-    const circle =document.querySelector('.circle');
-    circle.style.background = color;
-}*/
